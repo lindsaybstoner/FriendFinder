cdd2ee8d14285c0ae037e5fed442ddc5eb86da33
Blank <ellenmacy@Lindsays-MacBook-Pro.local>
2019-05-02 22:04:32 -0400

    added values

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-let PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
