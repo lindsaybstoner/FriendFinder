8a0bbeaf31577ab4f21b2e9d1e468071b4f0a0a4
Blank <ellenmacy@Lindsays-MacBook-Pro.local>
2019-05-04 16:30:14 -0400

    get the position of the best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,3 @@
-/* A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. */
 
 const friendData = require("../data/friends");
 
@@ -53,9 +51,12 @@ module.exports = function(app) {
             if (allTotalDiffs[k].totalDifference < bestMatch.totalDifference){
 
                 bestMatch.bestSpot = allTotalDiffs[k].referenceSpot;
-                
+
             }
         }
+
+        console.log(bestMatch);
+
 
     });
 }
